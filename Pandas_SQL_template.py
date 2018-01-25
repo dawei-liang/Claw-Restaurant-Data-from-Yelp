9f4c4f147dcba5a08536f5be501470c23ed07d59
dawei-liang <32776482+dawei-liang@users.noreply.github.com>
2018-01-25 11:41:25 -0600

    Add files via upload

---
+++ Pandas_SQL_template.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 15 15:04:07 2018
+
+@author: david
+"""
+
+import panda as pd
+import numpy as np
+
+df = pd.DataFrame({'rest_name': ['Salty Sow'],
+                   'rating':[4.5],
+                   'reviews_num':[1968]})
+
+
+
+
